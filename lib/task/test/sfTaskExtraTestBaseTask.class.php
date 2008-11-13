4a371a1581462660c161746e354e889253ca9382
Kris Wallsmith <kris.wallsmith@symfony-project.org>
2008-11-13 14:49:59 +0000

    [sfTaskExtraPlugin] added test:plugin task
    git-svn-id: http://svn.symfony-project.com/plugins/sfTaskExtraPlugin/trunk@12978 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ lib/task/test/sfTaskExtraTestBaseTask.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Base test task.
+ * 
+ * @package     sfTaskExtraPlugin
+ * @subpackage  task
+ * @author      Kris Wallsmith <kris.wallsmith@symfony-project.com>
+ * @version     SVN: $Id$
+ */
+abstract class sfTaskExtraTestBaseTask extends sfTaskExtraBaseTask
+{
+}
