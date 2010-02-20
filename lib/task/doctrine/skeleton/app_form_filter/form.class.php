e46b5ac68405b89c417e36567e8ca4aea6b29a97
Kris Wallsmith <kris.wallsmith@symfony-project.org>
2010-02-20 16:02:12 +0000

    [sfTaskExtraPlugin] added doctrine:build-app-filters task
    git-svn-id: http://svn.symfony-project.com/plugins/sfTaskExtraPlugin/branches/1.3@28153 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ lib/task/doctrine/skeleton/app_form_filter/form.class.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * ##MODEL## form filter class for the ##APPLICATION## application.
+ *
+ * @package    ##PROJECT##
+ * @subpackage filter
+ * @author     ##AUTHOR##
+ * @version    SVN: $Id$
+ */
+class ##APPLICATION####MODEL##FormFilter extends ##MODEL##FormFilter
+{
+  /**
+   * @see sfForm
+   */
+  public function configure()
+  {
+    parent::configure();
+  }
+}
